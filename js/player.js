142c2b8308ddcf56a50f63f30ac91aea9de29a2d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-05-04 10:02:52 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,20 +2,25 @@ const ID3 = window.jsmediatags;
 const sin = Math.sin;
 const π = Math.PI;
 var urlParameter = false;
-var maxrms = 0
+
 function ease(t) {
     return sin((t * π) / 2);
 }
+
 function draw(src) {
     var myRectangle = new Image();
     myRectangle.src = src;
-    myRectangle.onload = function () {};
+    myRectangle.onload = function() {};
     return myRectangle;
-}
+};
+
 function replaceurl(paramText) {
     var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + paramText;
-    window.history.pushState({ path: newurl }, "", newurl);
-}
+    window.history.pushState({
+        path: newurl
+    }, "", newurl)
+};
+
 function animate(myRectangle, canvas, context, startTime) {
     var time = new Date().getTime() - startTime;
     var amplitude = 150;
@@ -24,35 +29,31 @@ function animate(myRectangle, canvas, context, startTime) {
     var nextY = amplitude * sin((time * 2 * π) / period) + centerY;
     myRectangle.Y = nextY;
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(myRectangle);
-}
-function getGreaterInt(x,y) {
-  	if x > y {
-      	return x;
-    }else{
-      	return y;
-    };
+    context.drawImage(myRectangle)
 };
+
 function getRMS(arr) {
     var square = 0;
     var mean = 0;
-    var intrms = 0;
+    var rms = 0;
     var n = arr.length;
     for (var i = 0; i < n; i++) {
         square += Math.pow(arr[i], 2);
     };
     mean = square / n;
-    intrms = Math.sqrt(mean);
-  	maxrms = getGreaterInt(rms, maxrms)
-  	var rms = rms/maxrms
-    return rms *255;
-}
-window.addEventListener("load", function () {
+    rms = Math.sqrt(mean);
+    return rms
+};
+
+function calcRMSColor(rms) {
+    let intermed = rms / 100;
+    let ret = intermed * 150;
+    return ret
+};
+window.addEventListener("load", function() {
     var file = document.getElementById("thefile");
     var filetitle = document.getElementById("file-label");
-    document.getElementById(
-        "media-container"
-    ).innerHTML = `<canvas id="canvas"></canvas>\n<div id="main">\n    <div id="album">\n        <div id="MediaPlayerControls">\n            <div id="MediaPlayerIcon-icon-play" class="MediaPlayerIcon icon-play" data-mediathumb-url="src"></div>\n            <div id="sound_options" class="MediaPlayerIcon icon-volume">\n                <input id="volume" class="MediaPlayerControl-volume" type="range" max="100" min="0" />\n            </div>\n        </div>\n        <input id="MediaPlayerControl-seekbar" type="range" name="rng" min="0" value="0">\n       <div id="time-position"></div>\n    </div>\n</div>\n`;
+    document.getElementById("media-container").innerHTML = `<canvas id="canvas"></canvas>\n<div id="main">\n    <div id="album">\n        <div id="MediaPlayerControls">\n            <div id="MediaPlayerIcon-icon-play" class="MediaPlayerIcon icon-play" data-mediathumb-url="src"></div>\n            <div id="sound_options" class="MediaPlayerIcon icon-volume">\n                <input id="volume" class="MediaPlayerControl-volume" type="range" max="100" min="0" />\n            </div>\n        </div>\n        <input id="MediaPlayerControl-seekbar" type="range" name="rng" min="0" value="0">\n       <div id="time-position"></div>\n    </div>\n</div>\n <script src="../../js/rangeRunner.js"></script>\n`;
     replaceurl("player=" + urlParameter);
     var audio = new Audio();
     console.log(audio);
@@ -63,15 +64,16 @@ window.addEventListener("load", function () {
     var position = document.getElementById("time-position");
     var setting = document.getElementById("sound_options");
     var vol = document.getElementById("volume");
+
     function formatTime(val) {
         var min = Math.floor(val / 60);
         var sec = Math.floor(val - min * 60);
         if (sec < 10) {
             sec = "0" + sec;
-        }
-        return min + ":" + sec;
-    }
-    file.onchange = function () {
+        };
+        return min + ":" + sec
+    };
+    file.onchange = function() {
         var files = this.files;
         var colorValue = "#ff0000";
         dataimage.setAttribute("data-mediathumb-url", URL.createObjectURL(files[0]));
@@ -81,12 +83,12 @@ window.addEventListener("load", function () {
         var input = files[0].name;
         if (filetitle.textContent != "Unknown Artist - " + files[0].name) {
             filetitle.textContent = "Unknown Artist - " + files[0].name;
-        }
+        };
         if (album.style.backgroundImage != "url(../../images/default/default-album-icon.png)") {
             album.style.backgroundImage = "url(../../images/default/default-album-icon.png)";
-        }
+        };
         ID3.read(files[0], {
-            onSuccess: function (tag) {
+            onSuccess: function(tag) {
                 console.log(tag);
                 const data = tag.tags.picture.data;
                 const format = tag.tags.picture.format;
@@ -96,15 +98,15 @@ window.addEventListener("load", function () {
                     let str = "";
                     for (var o = 0; o < data.length; o++) {
                         str += String.fromCharCode(data[o]);
-                    }
+                    };
                     var url = "data:" + format + ";base64," + window.btoa(str);
                     album.style.backgroundImage = "url(" + url + ")";
-                }
+                };
                 if (title != "" && artist != "") {
                     filetitle.textContent = artist + " - " + title;
-                }
+                };
             },
-            onError: function (error) {
+            onError: function(error) {
                 console.log(error);
             },
         });
@@ -137,6 +139,7 @@ window.addEventListener("load", function () {
         var HEIGHT = canvas.height;
         var barWidth = WIDTH / bufferLength;
         var barHeight;
+
         function renderFrame() {
             requestAnimationFrame(renderFrame);
             analyser.getByteFrequencyData(dataArray);
@@ -151,60 +154,60 @@ window.addEventListener("load", function () {
             let rad = loud / 7;
             gn.gain.setValueAtTime(vol.value / 100, audio.currentTime);
             for (var i = 0; i < bufferLength; i++) {
-                /*barHeight = dataArray[i]*/ barHeight = (dataArray[i] / 255) * 60;
+                /*barHeight = dataArray[i]*/
+                barHeight = (dataArray[i] / 255) * 60;
                 ctx.save();
                 ctx.translate(centerX, centerY);
                 ctx.rotate(90 + i * ((Math.PI * 2) / bufferLength));
                 var r = (barHeight / 60) * 255 + 25 * (i / bufferLength);
                 var g = 250 * (i / bufferLength);
                 var b = 50;
-                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-                /*ctx.fillRect(0,0+rad, barWidth, barHeight/4.3)*/ ctx.fillRect(0, 0 + rad, barWidth, barHeight);
-                ctx.fillStyle = "rgb(255,255,255)";
-                /*ctx.fillRect(0,0+rad+barHeight/4.3, barWidth, 1)*/ ctx.fillRect(0, 0 + rad + barHeight, barWidth, 1);
+                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")"; /*ctx.fillRect(0,0+rad, barWidth, barHeight/4.3)*/
+                ctx.fillRect(0, 0 + rad, barWidth, barHeight);
+                ctx.fillStyle = "rgb(255,255,255)"; /*ctx.fillRect(0,0+rad+barHeight/4.3, barWidth, 1)*/
+                ctx.fillRect(0, 0 + rad + barHeight, barWidth, 1);
                 ctx.restore();
-            }
+            };
             ctx.beginPath();
             ctx.arc(centerX, centerY, rad, 0, Math.PI * 2, false);
-            ctx.fillStyle = "rgb(" + loud + ", " + loud + ",0)";
+            ctx.fillStyle = "rgb(" + calcRMSColor(loud) + ", " + calcRMSColor(loud) + ",0)";
             ctx.fill();
             ctx.closePath();
-        }
+        };
         renderFrame();
         audio.play();
-        dur.addEventListener("change", function () {
+        dur.addEventListener("change", function() {
             audio.currentTime = dur.value;
         });
-        setting.addEventListener("click", function () {
+        setting.addEventListener("click", function() {
             if (vol.hidden == true) {
                 vol.hidden = false;
             } else {
                 vol.hidden = true;
             }
         });
-        audio.addEventListener("timeupdate", function () {
+        audio.addEventListener("timeupdate", function() {
             dur.value = audio.currentTime;
             dur.max = audio.duration;
         });
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.className == "MediaPlayerIcon icon-pause") {
                 this.className = "MediaPlayerIcon icon-play";
-                audio.pause();
+                audio.pause()
             } else {
                 this.className = "MediaPlayerIcon icon-pause";
                 audio.play();
-            }
-            audio.addEventListener("ended", function () {
+            };
+            audio.addEventListener("ended", function() {
                 button.className = "MediaPlayerIcon icon-play";
                 dur.value = dur.max;
-              	maxrms = 0;
-            });
+            })
         });
-        audio.addEventListener("pause", function () {
-            button.className = "MediaPlayerIcon icon-play";
+        audio.addEventListener("pause", function() {
+            button.className = "MediaPlayerIcon icon-play"
         });
-        audio.addEventListener("play", function () {
+        audio.addEventListener("play", function() {
             button.className = "MediaPlayerIcon icon-pause";
-        });
-    };
+        })
+    }
 });
