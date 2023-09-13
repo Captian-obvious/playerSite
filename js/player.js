5ead37055ebcfc08c555d875795260c39b65416b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-13 12:13:41 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -57,7 +57,7 @@ window.addEventListener("load",(function() {
         return min + ":" + sec
     };
     audio.load();
-    file.addEventListener('change', (function() {
+    file.onchange=function() {
         var files = [];
         files = this.files;
         var index = 0;
@@ -222,5 +222,5 @@ window.addEventListener("load",(function() {
             requestAnimationFrame(renderFrame);
         };
         renderFrame();
-    }));
+    };
 }))
